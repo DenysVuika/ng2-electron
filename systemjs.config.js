075e8692e062dc6ffc7799a6e210867e61400d3a
Denys Vuika <denis.vuyka@gmail.com>
2016-09-08 19:27:21 +0100

    code cleanup
    - removes useless dependencies
    - migrates from ‘electron-prebuilt’ to ‘electron’

--- systemjs.config.js
+++ systemjs.config.js
@@ -7,7 +7,6 @@
     var map = {
         'app': 'app', // 'dist',
         '@angular': 'node_modules/@angular',
-        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
         'rxjs': 'node_modules/rxjs'
     };
     // packages tells the System loader how to load when no filename and/or no extension
@@ -18,11 +17,7 @@
         },
         'rxjs': {
             defaultExtension: 'js'
-        },
-        'angular2-in-memory-web-api': {
-            main: 'index.js',
-            defaultExtension: 'js'
-        },
+        }
     };
     var ngPackageNames = [
         'common',
