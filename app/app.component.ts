5e75eb210a92acd16b2445015d3c3efe618b1f34
Matheus Cardoso <matheus.mcas@gmail.com>
2016-09-07 23:29:11 -0300

    update code to the most recent RC6

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App for Electron</h1>'
+  template: '<h1>My First Angular 2 RC6 App with Electron</h1>'
 })
 export class AppComponent { }
