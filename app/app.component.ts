4dd0b0959ae92bcfe19c3daf2a2d8af77368ebcf
Denys Vuika <denis.vuyka@gmail.com>
2016-11-19 19:20:14 +0000

    Upgrade to Angular 2 final

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 RC6 App with Electron</h1>'
+  template: '<h1>My First Angular 2 App with Electron</h1>'
 })
 export class AppComponent { }
